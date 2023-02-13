0d0186de86d5ab0d6cddef2221afa6a4f226dd30
fulgurance <zohran.londais@gmail.com>
2023-02-13 21:07:38 +0000

    Patched Make-Ca to generate properly certificates

--- Make-Ca/1.7.0/1.7.0.cr
+++ Make-Ca/1.7.0/1.7.0.cr
@@ -3,15 +3,13 @@ class Target < ISM::Software
     def prepareInstallation
         super
         makeSource([Ism.settings.makeOptions,"DESTDIR=#{builtSoftwareDirectoryPath}#{Ism.settings.rootPath}","install"],buildDirectoryPath)
-        if Ism.softwareIsInstalled?(@information)
-            runMakeCaCommand(["-g"])
-        end
     end
 
     def install
         super
         makeDirectory("#{Ism.settings.rootPath}etc/ssl/local")
         setPermissions("#{builtSoftwareDirectoryPath}#{Ism.settings.rootPath}etc/ssl/local",0o755)
+        Ism.softwareIsInstalled?(@information) ? runMakeCaCommand(["-r"]) : runMakeCaCommand(["-g"])
     end
 
 end

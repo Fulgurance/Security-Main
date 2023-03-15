b1ae1230a030188a72b5efa343a2fa6721680fba
fulgurance <zohran.londais@gmail.com>
2023-03-15 13:11:55 +0000

    Patched sudo to be setup properly when Linux-Pam is installed

--- Sudo/1.9.7+p2/1.9.7+p2.cr
+++ Sudo/1.9.7+p2/1.9.7+p2.cr
@@ -2,6 +2,7 @@ class Target < ISM::Software
 
     def configure
         super
+
         configureSource([   "--prefix=/usr",
                             "--libexecdir=/usr/lib",
                             "--with-secure-path",
@@ -14,16 +15,31 @@ class Target < ISM::Software
 
     def build
         super
+
         makeSource([Ism.settings.makeOptions],buildDirectoryPath)
     end
     
     def prepareInstallation
         super
-        makeSource([Ism.settings.makeOptions,"DESTDIR=#{builtSoftwareDirectoryPath}/#{Ism.settings.rootPath}","install"],buildDirectoryPath)
+
+        makeSource([Ism.settings.makeOptions,"DESTDIR=#{builtSoftwareDirectoryPath}#{Ism.settings.rootPath}","install"],buildDirectoryPath)
+
+        if Dir.exists?("#{Ism.settings.rootPath}etc/pam.d")
+            makeDirectory("#{builtSoftwareDirectoryPath}#{Ism.settings.rootPath}etc/pam.d")
+
+            sudoData = <<-CODE
+            auth      include     system-auth
+            account   include     system-account
+            session   required    pam_env.so
+            session   include     system-session
+            CODE
+            fileWriteData("#{builtSoftwareDirectoryPath}#{Ism.settings.rootPath}etc/pam.d/sudo",sudoData)
+        end
     end
 
     def install
         super
+
         makeLink("libsudo_util.so.0.0.0","#{Ism.settings.rootPath}usr/lib/sudo/libsudo_util.so.0",:symbolicLinkByOverwrite)
     end
 

a774c37e615d32dc383ade6d99736a1893ffc30f
Fulgurance <zohran.londais@gmail.com>
2023-06-30 08:26:18 +0100

    Added gobject-introspection option

--- Polkit/0.119.0/0.119.0.cr
+++ Polkit/0.119.0/0.119.0.cr
@@ -16,6 +16,7 @@ class Target < ISM::Software
                             "--with-os-type=#{Ism.settings.systemName}",
                             "--disable-libsystemd-login",
                             "--disable-man-pages",
+                            "--enable-introspection=#{option("Gobject-Introspection") ? "yes" : "no"}",
                             "--enable-libelogind=#{option("Elogind") ? "yes" : "no"}",
                             "--with-authfw=#{option("Linux-Pam") ? "pam" : "shadow"}"],
                             buildDirectoryPath)

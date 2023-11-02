a47d02c88cb8bfb951dc379461e6db480ac8aa05
Fulgurance <zohran.londais@gmail.com>
2023-11-02 00:37:17 +0000

    Remove obsolete implementation

--- Openssl/1.1.1+l/1.1.1+l.cr
+++ Openssl/1.1.1+l/1.1.1+l.cr
@@ -12,13 +12,13 @@ class Target < ISM::Software
     
     def build
         super
-        makeSource([Ism.settings.makeOptions],buildDirectoryPath)
+        makeSource(path: buildDirectoryPath)
     end
     
     def prepareInstallation
         super
         fileReplaceText("#{buildDirectoryPath(false)}/Makefile","INSTALL_LIBS=libcrypto.a libssl.a","INSTALL_LIBS=")
-        makeSource([Ism.settings.makeOptions,"MANSUFFIX=ssl","DESTDIR=#{builtSoftwareDirectoryPath}/#{Ism.settings.rootPath}","install"],buildDirectoryPath)
+        makeSource(["MANSUFFIX=ssl","DESTDIR=#{builtSoftwareDirectoryPath}/#{Ism.settings.rootPath}","install"],buildDirectoryPath)
         moveFile("#{builtSoftwareDirectoryPath(false)}/#{Ism.settings.rootPath}/usr/share/doc/openssl","#{builtSoftwareDirectoryPath(false)}/#{Ism.settings.rootPath}/usr/share/doc/openssl-1.1.1l")
     end
 

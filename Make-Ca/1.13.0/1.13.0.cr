0a95ef42b48ce57414090fb30e05d50c347dbfd7
Fulgurance <zohran.londais@gmail.com>
2023-12-19 15:16:43 +0000

    Added make-ca 1.13.0 to fix server error with 1.12.0 version

---
+++ Make-Ca/1.13.0/1.13.0.cr
@@ -0,0 +1,18 @@
+class Target < ISM::Software
+    
+    def prepareInstallation
+        super
+
+        makeSource(["DESTDIR=#{builtSoftwareDirectoryPath}#{Ism.settings.rootPath}","install"],buildDirectoryPath)
+
+        makeDirectory("#{builtSoftwareDirectoryPath(false)}#{Ism.settings.rootPath}/etc/ssl/local")
+    end
+
+    def install
+        super
+
+        setPermissions("#{Ism.settings.rootPath}etc/ssl/local",0o755)
+        softwareIsInstalled("Make-Ca") ? runMakeCaCommand(["-r"]) : runMakeCaCommand(["-g"])
+    end
+
+end

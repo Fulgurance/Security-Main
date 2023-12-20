fff5ccf6dddddc9bc53efc3c202974b012e47c1f
Fulgurance <zohran.londais@gmail.com>
2023-12-20 18:40:26 +0000

    Added keyutils software

---
+++ Keyutils/1.6.1/1.6.1.cr
@@ -0,0 +1,27 @@
+class Target < ISM::Software
+
+    def prepare
+        super
+
+        fileReplaceText("#{buildDirectoryPath(false)}/Makefile","$(LIBDIR)/$(PKGCONFIG_DIR)","/usr/lib/pkgconfig")
+    end
+
+    def build
+        super
+
+        makeSource(path: buildDirectoryPath)
+    end
+
+    def prepareInstallation
+        super
+
+        makeSource(["NO_ARLIB=1",
+                    "LIBDIR=/usr/lib",
+                    "BINDIR=/usr/bin",
+                    "SBINDIR=/usr/sbin",
+                    "DESTDIR=#{builtSoftwareDirectoryPath}/#{Ism.settings.rootPath}",
+                    "install"],
+                    path: buildDirectoryPath)
+    end
+
+end

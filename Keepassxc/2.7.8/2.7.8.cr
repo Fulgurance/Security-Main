7bdc43a4f8e773d61780cea2227696446ea2ff2f
Fulgurance <zohran.londais@gmail.com>
2024-06-01 17:56:16 +0100

    Added new configuration

--- Keepassxc/2.7.8/2.7.8.cr
+++ Keepassxc/2.7.8/2.7.8.cr
@@ -11,6 +11,7 @@ class Target < ISM::Software
         runCmakeCommand([   "-DCMAKE_INSTALL_PREFIX=/usr",
                             "-DCMAKE_BUILD_TYPE=Release",
                             "-DWITH_XC_DOCS=OFF",
+                            "-DWITH_GUI_TESTS=OFF",
                             "-DWITH_TESTS=OFF",
                             ".."],
                             buildDirectoryPath)
